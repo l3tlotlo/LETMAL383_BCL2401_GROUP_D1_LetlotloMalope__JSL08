a6ea5589dc6b8723704ef4c768fd660a9cb6d4e7
Letlotlo Malope <Software.Engineer47@bafokeng.local>
2024-04-07 16:33:01 +0200

    JSL 08 initial push

---
+++ index.js
@@ -0,0 +1,27 @@
+class BankBranch {
+    constructor(branchInfo) {
+        if (!BankBranch.instance) {
+            this.branchInfo = "null";
+            BankBranch.instance = this;
+        }
+        return BankBranch.instance;
+    }
+
+    getBranchInfo() {
+        return this.branchInfo;
+    }
+}
+
+// Variables to store the singleton instance of the main bank branch
+const bankBranchInstance = new BankBranch({ name: "Main Branch", location: "City Center" });
+
+// Instances of the  class "BankBranch" with different branch information
+const branchA = new BankBranch({ name: "Branch A", location: "Sandton" });  //"Branch A" is located in "sandton"
+const branchB = new BankBranch({ name: "Branch B", location: "Hillbrow" }); //"Branch B" is located in "Hillbrow"
+
+// Retrieving branch information from the instances using "getBranchInfo", output for both should be { name: "Main Branch", location: "City Center" }
+console.log(branchA.getBranchInfo()); 
+console.log(branchB.getBranchInfo());  
+
+// Verifying that `branchA` and `branchB` are both referring to the same instance using "===" (truthy/falsy)
+console.log(branchA === branchB); // Output: true
